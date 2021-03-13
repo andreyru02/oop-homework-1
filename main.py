18b56056d10513e36b47f5b39ecdac179982ffdd
Andrey Chebunin <andrey.tchebunin@yandex.ru>
2021-03-14 01:50:23 +0800

    Task 3 and fix task 2

--- main.py
+++ main.py
@@ -6,15 +6,43 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        self.grades_lecturer = []
+
+    def __str__(self):
+        name = self.name
+        surname = self.surname
+
+        for grade in self.grades.values():
+            self.score = grade
+
+        for courses_p in self.courses_in_progress:
+            self.courses_progress = courses_p
+
+        if len(self.finished_courses) == 0:
+            self.courses_finished = 'Завершенные курсы отсутствуют.'
+        else:
+            for courses_f in self.finished_courses:
+                self.courses_finished = courses_f
+
+        self.rating = round(sum(self.score) / len(self.score), 1)
+        return f'Имя: {self.name}\n' \
+               f'Фамилия: {self.surname}\n' \
+               f'Средняя оценка за домашние задания: {self.rating}\n' \
+               f'Курсы в процессе изучения: {self.courses_progress}\n' \
+               f'Завершенные курсы: {self.courses_finished}'
+
+    def __gt__(self, other):
+        return self.rating > other.rating
 
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
 
     def rate_lecturer(self, lecturer, student, course, grade):
         if isinstance(student, Student) and course in lecturer.courses_attached and course in student.courses_in_progress:
-            if grade <= 10:
-                student.grades_lecturer.append(grade)
+            if 10 >= grade >= 1:
+                if course in lecturer.grades:
+                    lecturer.grades[course] += [grade]
+                else:
+                    lecturer.grades[course] = [grade]
             else:
                 return 'Оценка выставляется по 10 балльной шкале'
         else:
@@ -38,11 +66,28 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+    def __str__(self):
+        name = self.name
+        surname = self.surname
+
+        for grades in self.grades.values():
+            self.score = grades
+
+        self.rating = round(sum(self.score) / len(self.score), 1)
+        return f'Имя: {name}\nФамилия: {surname}\nСредняя оценка за лекции: {self.rating}'
+
+    def __gt__(self, other):
+        return self.rating > other.rating
 
 
 class Reviewer(Mentor):
-    pass
+    def __str__(self):
+        name = self.name
+        surname = self.surname
+        return f'Имя: {name}\nФамилия: {surname}'
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
@@ -57,10 +102,20 @@ cool_reviewer.rate_hw(best_student, 'Python', 10, cool_reviewer)
 
 best_lecturer = Lecturer('Olga', 'Voronina')
 best_lecturer.courses_attached += ['Python']
+
 best_student.rate_lecturer(best_lecturer, best_student, 'Python', 10)
 best_student.rate_lecturer(best_lecturer, best_student, 'Python', 10)
 best_student.rate_lecturer(best_lecturer, best_student, 'Python', 10)
 
+lecturer = Lecturer('User', 'User')
+lecturer.courses_attached += ['Python']
+
+best_student.rate_lecturer(lecturer, best_student, 'Python', 9)
+best_student.rate_lecturer(lecturer, best_student, 'Python', 10)
+best_student.rate_lecturer(lecturer, best_student, 'Python', 10)
 
 print(best_student.grades)
-print(best_student.grades_lecturer)
+print(best_lecturer.grades)
+print(cool_reviewer)
+print(best_lecturer)
+print(best_student)
